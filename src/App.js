44a09cd20e9327177b9a048fa0be2119c36e21b1
jisu Seo <jseo9732@gmail.com>
2023-08-23 00:48:48 +0900

    Ch 14 - 03. 컴포넌트 - 선언적 렌더링과 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { Component } from './core/heropy';
 export default class App extends Component {
-  //   constructor() {  // 생략 가능
-  //     super();
-  //   }
+  constructor() {
+    super({ state: { inputText: '' } });
+  }
   render() {
-    this.el.textContent = 'Hello, world!';
+    this.el.classList.add('search');
+    this.el.innerHTML = /* html */ `
+        <input />
+        <button>Click!</button>
+    `;
+
+    const inputEl = this.el.querySelector('input');
+    inputEl.addEventListner('input', () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector('button');
+    buttonEl.addEventListner('click', () => {
+      console.log(this.state.inputText);
+    });
   }
 }
