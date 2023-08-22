b3c8f0eb569c60a8ea5935a56aa69d5dd84309e3
jisu Seo <jseo9732@gmail.com>
2023-08-23 01:25:53 +0900

    Revert "Ch 14 - 04. 컴포넌트 - 조건과 반복"
    This reverts commit 4943abcfd7dbc8865ba5803350c6774906ef474a.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import { Component } from './core/heropy';
 export default class App extends Component {
   constructor() {
-    super({
-      state: {
-        fruits: [
-          { name: 'Apple', price: 1000 },
-          { name: 'Banana', price: 2000 },
-          { name: 'Cherry', price: 3000 },
-        ],
-      },
-    });
+    super({ state: { inputText: '' } });
   }
   render() {
-    console.log(this.state.fruits);
-
+    this.el.classList.add('search');
     this.el.innerHTML = /* html */ `
-      <h1>Fruits</h1>
-      <ul>
-      ${this.state.fruits
-        .filter((fruit) => fruit.price < 3000)
-        .map((fruit) => `<li>${fruit.name}</li>`)
-        .join('')}
-  </ul>
+        <input />
+        <button>Click!</button>
     `;
+
+    const inputEl = this.el.querySelector('input');
+    inputEl.addEventListner('input', () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector('button');
+    buttonEl.addEventListner('click', () => {
+      console.log(this.state.inputText);
+    });
   }
 }
